101974982ab6501868d30df1bd2d5ec2b2046d68
i74ifa <i74ifa@gmail.com>
2021-12-02 23:15:16 +0300

    symlink to /usr/local/bin

--- bin/vaid.php
+++ bin/vaid.php
@@ -6,6 +6,17 @@ use I74ifa\Vaid\Bash\Color;
 use I74ifa\Vaid\Vaid;
 
 
+
+// check if installed
+
+$bins = scandir('/usr/local/bin/');
+
+if (!in_array('vaid', $bins)){
+    echo Color::green("symlink to users bin to /usr/local/bin:\n");
+    $dir = dirname(__DIR__) . '/vaid';
+    shell_exec("sudo ln -snf $dir /usr/local/bin/");
+}
+
 // if user is root
 if (posix_getuid() != 0) {
     echo Color::red("run as sudo\n");
