7afa8e79121390b97c15682809caa3aeebd17ff8
i74ifa <i74ifa@gmail.com>
2022-01-13 17:29:02 +0300

    function_exists and v2.0.2

--- helpers.php
+++ helpers.php
@@ -2,16 +2,19 @@
 
 use Symfony\Component\Process\Process;
 
-function should_be_sudo()
-{
-    if (!isset($_SERVER['SUDO_USER'])){
-        throw new Exception('This command must be run with sudo.');
+if (! function_exists('should_be_sudo')) {
+    function should_be_sudo()
+    {
+        if (!isset($_SERVER['SUDO_USER'])){
+            throw new Exception('This command must be run with sudo.');
+        }
     }
 }
 
-
-function dd($data)
-{
-    print_r($data);
-    exit;
+if (! function_exists('dd')) {
+    function dd($data)
+    {
+        print_r($data);
+        exit;
+    }
 }
